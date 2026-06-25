e6e9f4a5429a9b7743f96c05874132e34bad60b5
Nycelinux <celina.ludwigs@fh-erfurt.de>
2026-06-25 03:23:31 +0200

    bug fixes

--- Script1.js
+++ Script1.js
@@ -3,11 +3,11 @@ let level = 0;
 let xp = 0;
 const desk = document.getElementById("desk-hitbox");
 desk.addEventListener("click", () => {
-    document.getElementById("timer-menue").classList.remove(hidden);
+    document.getElementById("timer-menu").classList.remove("hidden");
 });
 
 function closeMenu() {
-    document.getElementById("timer-menu").classList.add(hidden);
+    document.getElementById("timer-menu").classList.add("hidden");
 }
 
 function addXP(amount) {
@@ -25,7 +25,7 @@ function addXP(amount) {
 function startTimer(seconds, rewardXP) {
     closeMenu();
     const timerWindow = document.getElementById("timer-window");
-    timerWindow.classList.remove(hidden);
+    timerWindow.classList.remove("hidden");
     let time = seconds;
     const interval = setInterval(() => {
         const minutes = Math.floor(time / 60);
@@ -41,24 +41,26 @@ function startTimer(seconds, rewardXP) {
         }
     }, 1000);
 
-    function saveGame() {
-        localStorage.setItem("level", level);
-        localStorage.setItem("xp", xp);
-    }
-
-    function loadGame() {
-        const savedLevel = localStorage.getItem("level");
-        const savedXP = localStorage.getItem("xp");
-        if (savedXP) {
-            xp = Number(savedXP);
-            document.getElementById("xp").innerText = xp;
-        }
-        if (savedLevel) {
-            level = Number(savedLevel);
-            document.getElementById("level").innerText = level;
-        }
-    }
-
-    loadGame();
+    
 
 }
+
+function saveGame() {
+    localStorage.setItem("level", level);
+    localStorage.setItem("xp", xp);
+}
+
+function loadGame() {
+    const savedLevel = localStorage.getItem("level");
+    const savedXP = localStorage.getItem("xp");
+    if (savedXP) {
+        xp = Number(savedXP);
+        document.getElementById("xp").innerText = xp;
+    }
+    if (savedLevel) {
+        level = Number(savedLevel);
+        document.getElementById("level").innerText = level;
+    }
+}
+
+loadGame();
