4f89be6bfdbe666f1803f3bbbf7aa15927de1c62
Nycelinux <celina.ludwigs@fh-erfurt.de>
2026-07-09 01:04:16 +0200

    furnitureDatabse addded, new folders for placement added

---
+++ js/furniture/furnitureDatabase.js
@@ -0,0 +1,27 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * furnitureDatabase
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+const furnitureDatabase = {
+    plantPot: {
+        id: "plantPot",
+        name: "Blumentopf",
+        image: "plantPot.png",
+        width: 48,
+        height: 48
+    },
+    chair: {
+        id: "chair",
+        name: "Stuhl",
+        image: "chair.png",
+        width:48,
+        height:48
+    },
+    desk: {
+        id: "desk",
+        name: "Schreibtisch",
+        image: "desk.png",
+        width:96,
+        height:64
+    }
+}
